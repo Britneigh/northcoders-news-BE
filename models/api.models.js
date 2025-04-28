bfecf73f43bdb34423eaa3d2092dba6d046294ff
Britney Nguyen <britneynguyen18@yahoo.com>
2025-04-28 11:54:20 +0100

    completed ticket 1: GET/api

---
+++ models/api.models.js
@@ -0,0 +1,8 @@
+const db = require(".././db/connection");
+
+// const selectEndpoints = () => {
+//     return db.query(`SELECT * FROM `)
+//     .then ((result) => {
+//         return result.rows;
+//     });
+// }
