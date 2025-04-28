a8a7312a621f5938e3be43acfaa1567ee84cc350
Britney Nguyen <britneynguyen18@yahoo.com>
2025-04-28 13:46:04 +0100

    completed ticket 2: GET /api/topics

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -1,8 +1,15 @@
-//const {} =  require("../models/api.models");
+const { selectTopics } =  require("../models/api.models");
 const endpoints = require("../endpoints.json");
 
 const getApi = (req, res) => {
     res.status(200).send({endpoints: endpoints});
 };
 
-module.exports = { getApi };
+const getTopics = (req, res) => {
+    return selectTopics()
+    .then((topics) => {
+        console.log(topics);
+        res.status(200).send({topics: topics});
+    });
+}
+module.exports = { getApi, getTopics };
