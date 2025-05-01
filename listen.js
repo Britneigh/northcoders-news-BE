8b5e7acd7a8058613732c3cc49ed7b5be512da3a
Britney Nguyen <britneynguyen18@yahoo.com>
2025-05-01 11:07:06 +0100

    Supabase setup

--- listen.js
+++ listen.js
@@ -1,9 +1,10 @@
 const app = require("./app.js");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, ()=>{
+app.listen(PORT, () => {
     if(err){
         console.log(err)
     } else {
-    console.log("Listening on port 9090");
+    console.log(`Listening on ${PORT}...`);
     }
 })
